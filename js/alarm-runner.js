d1bff6804c20a22fc78b6a10f0a35538412bf4c9
Arend de Jonge <arrowacrobatics@gmail.com>
2023-09-15 13:32:03 +0200

    implement coarse alarm runner

--- js/alarm-runner.js
+++ js/alarm-runner.js
@@ -1,13 +1,15 @@
+import * as utils from "./utils.js";
+
 /**
  * Responsible for a single alarm instance/moment.
  * Deals with the button UI and operates the VLC bridge.
  */
 export class AlarmRunner {
-    constructor(alarmSettingsJson, vlcBridge, settings) {
+    constructor(settings, appEvents, alarmSettingsJson) {
         // TODO: add scheduler ref for querying current run status.
         this._settings = settings;
         this._alarmConf = alarmSettingsJson;
-        this._vlcBridge = vlcBridge;
+        this._events = appEvents;
     }
 
     async run() {
@@ -15,20 +17,26 @@ export class AlarmRunner {
             console.log("Running an alarm at " + new Date());
             console.log(this._alarmConf);
         }
+        this._events.emit('alarmpi-start', this._alarmConf);
 
         return new Promise(async (resolve, reject) => {
             // let timeoutP = sleep(maxAlarmTime).then(() => {this.stop; resolve();}));
             // buttonUI.on('stopButton', () => {this.stop; resolve();}));
             // buttonUI.on('snoozeButton' () => { this.snooze();});
-            await utils.sleep(1000);
+            // Use Promise.race(sleep, snooze, stop)?
+
+            await utils.sleep(1000); // debug
+
             if (this._settings.verbose) {
                 console.log("alarm done");
             }
+            this.stop();
             resolve();
         });
     }
 
     stop() {
-        // for other alarms to go solo.
+        // for other alarms to go solo etc.
+        this._events.emit('alarmpi-stop', this._alarmConf);
     }
 };
