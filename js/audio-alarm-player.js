bbea3db4dd62587221100dd13764ddf76c9dfe55
Johnny Arrow <arrowacrobatics@gmail.com>
2023-10-11 13:24:00 +0200

    Update logging

--- js/audio-alarm-player.js
+++ js/audio-alarm-player.js
@@ -17,19 +17,21 @@ export class AlarmPlayer {
 
     enableEventHandlers() {
         this._events.on('alarmpi-reload', async (alarmsettings) => {
-            console.log(`AlarmPlayer.alarmpi-reload: ${alarmsettings}`)
+            console.log(`AlarmPlayer.alarmpi-reload: stop`)
             await this._vlc.stop();
+            console.log(`AlarmPlayer.alarmpi-reload: clear`)
             await this._vlc.clear();
+            console.log(`AlarmPlayer.alarmpi-reload: load`)
             await this.loadPlaylist();
         });
 
         this._events.on('alarmpi-start', async (alarmsettings) => {
-            console.log(`AlarmPlayer.alarmpi-start: ${alarmsettings}`)
+            console.log(`AlarmPlayer.alarmpi-start: play`)
             await this._vlc.play().catch(e => console.log(`Vlc failed "start": ${e}`));
         });
 
         this._events.on('alarmpi-stop', async (alarmsettings) => {
-            console.log(`AlarmPlayer.alarmpi-stop ${alarmsettings}`)
+            console.log(`AlarmPlayer.alarmpi-stop stop`)
             await this._vlc.stop().catch(e => console.log(`Vlc failed "stop": ${e}`));
         });
 
